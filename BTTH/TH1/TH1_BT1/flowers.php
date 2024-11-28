37828dd6979702078a16ce847b79170264ba6b18
buivandong12 <167645163+buivandong12@users.noreply.github.com>
2024-11-28 21:32:14 +0700

    CNWeb

---
+++ BTTH/TH1/TH1_BT1/flowers.php
@@ -0,0 +1,28 @@
+<?php
+$flowers = array (
+  0 => 
+  array (
+    'name' => 'Hoa hướng dương',
+    'description' => 'biểu tượng của mặt trời',
+    'image' => 'images/hoa2.jpg',
+  ),
+  1 => 
+  array (
+    'name' => 'Hoa sen',
+    'description' => 'đẹp',
+    'image' => 'images/hoa8.webp',
+  ),
+  2 => 
+  array (
+    'name' => 'hoa cẩm trướng',
+    'description' => 'đẹp ác',
+    'image' => 'images/hoa7.webp',
+  ),
+  3 => 
+  array (
+    'name' => 'hoa giấy',
+    'description' => 'bình thường',
+    'image' => 'images/hoa4.webp',
+  ),
+);
+?>
